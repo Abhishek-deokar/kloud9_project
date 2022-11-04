adac200821a2bd2f78f204f2bbf59fec750e6f71
Abhi_deokar <114213916+Abhishek-deokar@users.noreply.github.com>
2022-11-04 09:18:20 +0530

    Update cleansed_layer.py

--- modules(.py)/cleansed_layer.py
+++ modules(.py)/cleansed_layer.py
@@ -2,11 +2,12 @@ from pyspark.sql import *
 from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master('local').appName('clans_layer').getOrCreate()
-raw_df = spark.read.format('csv').load("s3a://s3-sink-abhi//raw_df//part-00000*.csv")
+# raw_df = spark.read.format('csv').load("s3a://s3-sink-abhi//raw_df//part-00000*.csv")
+raw_df = spark.read.format("csv").option("header","True").load("C:\\Users\\abhishek.dd\\Desktop\\Anmol\\git\\raw")
 raw_df.show()
 
 spe_char = r'[%,|"-&.=?-]'
-
+raw_df = raw_df.na.fill("NA")
 ######## CLEANING THE DATA WITH DATATYPE AND ADDING NEW COLUMN ***********************
 cleansed = raw_df.withColumn("id", col('id').cast('int')) \
     .withColumn("datetime_confirmed", to_timestamp("datetime_confirmed", 'dd/MMM/yyyy:HH:mm:ss')) \
@@ -14,13 +15,15 @@ cleansed = raw_df.withColumn("id", col('id').cast('int')) \
     .withColumn('size', col('size').cast('int')) \
     .withColumn("request", regexp_replace("request", spe_char, "")) \
     .withColumn("size", round(col("size") / 1024, 2)) \
-    .withColumn('referer_present', when(col('referer') == '', 'N') \
+    .withColumn('referer_present', when(col('referer') == "NA", 'N') \
                 .otherwise('Y'))
-cleansed = cleansed.withColumn("datetime_confirmed", split(cleansed["datetime_confirmed"], ' ')\
-                    .getItem(0)).withColumn("datetime_confirmed",to_timestamp("datetime_confirmed",'dd/MMM/yyyy:hh:mm:ss'))\
-                                .withColumn("datetime_confirmed",to_timestamp("datetime_confirmed",'MMM/dd/yyyy:hh:mm:ss'))
+
+
 cleansed.printSchema()
-cleansed.na.fill("NA").show()
-#
+cleansed.show(truncate=False)
+print(cleansed.count())
+
 # cleansed.write.format("csv").mode('overwrite').save('s3a://s3-sink-abhi//cleansed')
-# cleansed.write.mode('overwrite').saveAsTable("cleansed")
+cleansed.write.mode('overwrite').saveAsTable("cleansed")
+spark.sql("show tables;").show()
+spark.sql("select count(*) from cleansed;").show()
