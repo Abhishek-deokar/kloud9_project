5a1b0d2d19c936feb1a1617b659538539100b541
Abhi_deokar <114213916+Abhishek-deokar@users.noreply.github.com>
2022-11-08 14:34:01 +0530

    Update and rename increase_shared_input.py to increase_data_input.py

--- modules(.py)/increase_data_input.py
+++ modules(.py)/increase_data_input.py
@@ -6,20 +6,19 @@ new_l=Path('C:\\Users\\abhishek.dd\\Desktop\\Anmol\\Anmol_arrived\\again_new.txt
 
 p=open(l)
 count = 0
-increment = 1
-increm = 1
 new_data=open(new_l,'a')
 
 def incremental():
 
     p.seek(0)
     for line_num, i in enumerate(p):
-        global increment
         global count
-        global increm
+        
         l_digit = int(i.strip(" ").split(" ")[0].split(".")[-1])
         time_hour = int(i.strip(" ").split(" ")[3].split(":")[1])
         day_digit = int(i.strip(" ").split(" ")[3].split("/")[0][1:])
+        
+       
         if line_num < 300:
                 if (line_num + 1) % 9 == 0:
                     i = i.replace('GET', 'POST', 1)
@@ -27,14 +26,9 @@ def incremental():
                     i = i.replace(":0"+str(time_hour), ":0"+str(b),1)
 
                 if (line_num + 1) % 10 == 0:
-                    increment += 1
-                    increm += 1
-                    # incr = int(l_digit + increment)
                     a = randbelow(999)
                     i = i.replace(str(l_digit),str(a),1)
                     b = randbelow(12)
-                    # i = i.replace(str(l_digit), str(incr), 1)
-                    # incr_hour = int(time_hour + increm)
                     i = i.replace(":0"+str(time_hour), ":0"+str(b),1)
 
                 if (line_num + 1) % 11 == 0:
@@ -44,8 +38,6 @@ def incremental():
         new_data.write(i)
         print(count)
         count = count + 1
-        # increment = +1
-        # increm = 1
 
 num_record = int(input("Enter the number of records wanted(in Lakhs): "))
 num_acc = int((num_record * 1000))
