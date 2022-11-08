9793c7c8f11c3f751211a55e607bdfa73fece836
Abhi_deokar <114213916+Abhishek-deokar@users.noreply.github.com>
2022-11-08 14:08:44 +0530

    Update cleansed_layer.py

--- modules(.py)/cleansed_layer.py
+++ modules(.py)/cleansed_layer.py
@@ -14,7 +14,7 @@ cleansed = raw_df.withColumn("id", col('id').cast('int')) \
     .withColumn('status_code', col('status_code').cast('int')) \
     .withColumn('size', col('size').cast('int')) \
     .withColumn("request", regexp_replace("request", spe_char, "")) \
-    .withColumn("size", round(col("size") / 1024, 2)) \
+    .withColumn("size", round(col("size") / 1024)) \
     .withColumn('referer_present', when(col('referer') == "NA", 'N') \
                 .otherwise('Y'))
 
@@ -23,7 +23,7 @@ cleansed.printSchema()
 cleansed.show(truncate=False)
 print(cleansed.count())
 
-# cleansed.write.format("csv").mode('overwrite').save('s3a://s3-sink-abhi//cleansed')
+# cleansed.repartition(1).write.format("csv").mode('overwrite').save('s3a://s3-sink-abhi//cleansed')
 cleansed.write.mode('overwrite').saveAsTable("cleansed")
-spark.sql("show tables;").show()
-spark.sql("select count(*) from cleansed;").show()
+spark.sql("show tables").show()
+spark.sql("select count(*) from cleansed").show()
