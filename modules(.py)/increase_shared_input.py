d1b4bfa3f4014bf4f2f89807602444b581cf2618
Abhi_deokar <114213916+Abhishek-deokar@users.noreply.github.com>
2022-11-08 14:20:28 +0530

    Update increase_shared_input.py

--- modules(.py)/increase_shared_input.py
+++ modules(.py)/increase_shared_input.py
@@ -1,6 +1,8 @@
-from pathlib import Path
+om pathlib import Path
+from secrets import randbelow
+import random
 l=Path('C:\\Users\\abhishek.dd\\Desktop\\Anmol\\Anmol_arrived\\log_data_ip_request.txt')
-new_l=Path('C:\\Users\\abhishek.dd\\Desktop\\Anmol\\Anmol_arrived\\new_log.txt')
+new_l=Path('C:\\Users\\abhishek.dd\\Desktop\\Anmol\\Anmol_arrived\\again_new.txt')
 
 p=open(l)
 count = 0
@@ -17,18 +19,33 @@ def incremental():
         global increm
         l_digit = int(i.strip(" ").split(" ")[0].split(".")[-1])
         time_hour = int(i.strip(" ").split(" ")[3].split(":")[1])
-        if (line_num + 1) % 10 == 0:
-            incr = int(l_digit + increment)
-            i = i.replace(str(l_digit), str(incr), 1)
-            incr_hour = int(time_hour + increm)
-            i = i.replace(":0"+str(time_hour), ":0"+str(incr_hour))
-        if (line_num + 1) % 9 == 0:
-            i = i.replace('GET', 'POST', 1)
+        day_digit = int(i.strip(" ").split(" ")[3].split("/")[0][1:])
+        if line_num < 300:
+                if (line_num + 1) % 9 == 0:
+                    i = i.replace('GET', 'POST', 1)
+                    b = randbelow(12)
+                    i = i.replace(":0"+str(time_hour), ":0"+str(b),1)
+
+                if (line_num + 1) % 10 == 0:
+                    increment += 1
+                    increm += 1
+                    # incr = int(l_digit + increment)
+                    a = randbelow(999)
+                    i = i.replace(str(l_digit),str(a),1)
+                    b = randbelow(12)
+                    # i = i.replace(str(l_digit), str(incr), 1)
+                    # incr_hour = int(time_hour + increm)
+                    i = i.replace(":0"+str(time_hour), ":0"+str(b),1)
+
+                if (line_num + 1) % 11 == 0:
+                    c = random.randint(10,31)
+                    i = i.replace(str(day_digit),str(c),1)
+
         new_data.write(i)
         print(count)
         count = count + 1
-        increment = +1
-        increm = 1
+        # increment = +1
+        # increm = 1
 
 num_record = int(input("Enter the number of records wanted(in Lakhs): "))
 num_acc = int((num_record * 1000))
